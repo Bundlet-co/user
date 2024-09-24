fa3cc977db1fcda85ab531e990865b935032a2af
dev_apus <folorunsopraise580@gmail.com>
2024-09-24 11:12:56 +0100

    PWA integration

--- vite.config.js
+++ vite.config.js
@@ -13,17 +13,17 @@ const vitePWA = VitePWA({
   theme_color: "#ffffff",
   icons: [
     {
-      src: "/favicon.ico",
+      src: "./favicon.ico",
       sizes: "192x192",
       type: "image/png"
     },
     {
-      src: "/favicon.ico",
+      src: "./favicon.ico",
       sizes: "512x512",
       type: "image/png"
     },
     {
-      src: "/favicon.ico",
+      src: "./favicon.ico",
       sizes: "64x64 32x32 24x24 16x16",
       type: "image/x-icon"
     }
