867670f127143d176f3fa6f3789dc0ae0e0df90d
dev_apus <folorunsopraise12@gmail.com>
2025-09-11 15:33:45 +0100

    done

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-bb28c478'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9aok8rb16co"
+    "revision": "0.5qiq9e6nikc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
